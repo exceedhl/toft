853886206b3b0441b383f3b2c046ace98fcbf3d3
Francisco Trindade <frank.trindade@gmail.com>
2012-02-03 20:02:03 +1100

    Fixing unit test

--- spec/toft/node_spec.rb
+++ spec/toft/node_spec.rb
@@ -6,7 +6,7 @@ describe "Node" do
 
     it 'should run block with puppet runner' do
       puppet_runner = mock Toft::Puppet::PuppetRunner.class
-      puppet_runner.should_receive(:run).with('manifest')
+      puppet_runner.should_receive(:run).with('manifest', {})
       
       node = Toft::Node.new("my_host", {:runner => puppet_runner})
       node.run_puppet('manifest')
